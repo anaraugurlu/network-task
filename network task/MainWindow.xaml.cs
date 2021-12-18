1282c4f85d1dcbe38e10e0fcc3d8f7b32685891c
Ugur_ml97 <Ugur_ml97@itstep.edu.az>
2021-12-18 16:28:31 +0400

    Add project files.

---
+++ network task/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace network_task
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+        
+           
+        
+            private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            //string urlAddress = txt.Text  ;
+
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //if (response.StatusCode == HttpStatusCode.OK)
+            //{
+            //    Stream receiveStream = response.GetResponseStream();
+            //    StreamReader readStream = null;
+            //    if (response.CharacterSet == null)
+            //    {
+            //        readStream = new StreamReader(receiveStream);
+            //    }
+            //    else
+            //    {
+            //        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+            //        string data = readStream.ReadToEnd();
+            //        response.Close();
+            //        readStream.Close();
+            //    }
+            //}
+
+ string url = txt.Text;
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(url);
+            myRequest.Method = "GET";
+            WebResponse myResponse = myRequest.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            string result = sr.ReadToEnd();
+            sr.Close();
+            myResponse.Close();
+            if (result.StartsWith("<") && result.EndsWith("a"))
+            {
+
+
+            }
+             
+               
+            }
+    }
+    }
+
